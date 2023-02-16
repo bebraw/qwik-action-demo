4c1a901414f30635e8863229cc6330624a56e156
Juho Vepsäläinen <bebraw@gmail.com>
2023-02-16 15:22:27 +0200

    chore: Set up a minimal db example

--- src/components/rating/rating.tsx
+++ src/components/rating/rating.tsx
@@ -1,7 +1,33 @@
-import { component$,  } from '@builder.io/qwik';
+import { promises as fs } from "fs";
+import { action$, Form } from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
 
-export default component$(() => {
+export const useSetRating = action$(async (form) => {
+  console.log("received form", form);
+
+  await fs.writeFile(
+    "db.txt",
+    JSON.stringify({ rating: form.rating }, null, 2),
+    "utf8"
+  );
+
+  return {
+    success: true,
+  };
+});
+
+export default component$(({ initialRating }: { initialRating: string }) => {
+  const action = useSetRating();
+
+  console.log("received initial rating", initialRating);
+
   return (
-    <div>Rating goes here</div>
+    <Form action={action}>
+      <input
+        name="rating"
+        value={action.formData?.get("rating") || initialRating}
+      />
+      <button type="submit">Set rating</button>
+    </Form>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,35 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import Rating from '../components/rating/rating'
+import { promises as fs } from "fs";
+import { component$ } from "@builder.io/qwik";
+import { loader$ } from "@builder.io/qwik-city";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import Rating from "../components/rating/rating";
+
+export const useLoadRating = loader$(async () => {
+  const { rating } = JSON.parse(
+    await fs.readFile("db.txt", { encoding: "utf8" })
+  );
+
+  console.log("loaded rating", rating);
+
+  return rating;
+});
 
 export default component$(() => {
+  const signal = useLoadRating();
+
   return (
     <div>
-      <Rating />
+      <Rating initialRating={signal.value} />
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Rating demo",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
